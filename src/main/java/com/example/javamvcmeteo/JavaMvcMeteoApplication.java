237088a594a4e509ddbeb8c1057e13c8e54b30df
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2024-03-12 11:45:02 +0200

    pulling json

--- src/main/java/com/example/javamvcmeteo/JavaMvcMeteoApplication.java
+++ src/main/java/com/example/javamvcmeteo/JavaMvcMeteoApplication.java
@@ -1,5 +1,6 @@
 package com.example.javamvcmeteo;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -8,6 +9,7 @@ public class JavaMvcMeteoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaMvcMeteoApplication.class, args);
+
     }
 
 }
--- src/main/java/com/example/javamvcmeteo/controllers/ForecastController.java
+++ src/main/java/com/example/javamvcmeteo/controllers/ForecastController.java
@@ -7,14 +7,46 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Scanner;
 
 @Controller
 public class ForecastController {
     @GetMapping("/")
-    public ModelAndView index() {
+    public ModelAndView index() throws IOException {
         ModelAndView modelAndView = new ModelAndView("index");
 
+        var forecasts = getForecastModels();
+        var meteoForecastsJson = getMeteoForecastsJson();
+        System.out.println(meteoForecastsJson);
+
+        modelAndView.addObject("forecasts", forecasts);
+
+        return modelAndView;
+    }
+
+    public static String getMeteoForecastsJson() throws IOException {
+        URL url = new URL("https://api.meteo.lt/v1/places/vilnius/forecasts/long-term");
+
+        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+        conn.setRequestMethod("GET");
+        conn.connect();
+
+        String text = "";
+        Scanner scanner = new Scanner(url.openStream());
+        while (scanner.hasNext()) {
+            text += scanner.nextLine();
+        }
+        scanner.close();
+        return text;
+
+    }
+
+    private static ArrayList<ForecastModel> getForecastModels() {
         var forecasts = new ArrayList<ForecastModel>();
         var row1 = new ForecastModel("2024-03-12 11:00", 1.0);
         var row2 = new ForecastModel("2024-03-12 12:00", 4.2);
@@ -23,9 +55,6 @@ public class ForecastController {
         forecasts.add(row1);
         forecasts.add(row2);
         forecasts.add(row3);
-
-        modelAndView.addObject("forecasts", forecasts);
-
-        return modelAndView;
+        return forecasts;
     }
 }
