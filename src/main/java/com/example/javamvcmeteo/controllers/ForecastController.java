2e08314d874cff2b247215ac98b5807e6428cb81
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2024-03-12 10:48:16 +0200

    Get data from Java

--- src/main/java/com/example/javamvcmeteo/controllers/ForecastController.java
+++ src/main/java/com/example/javamvcmeteo/controllers/ForecastController.java
@@ -1,16 +1,31 @@
 package com.example.javamvcmeteo.controllers;
 
+import com.example.javamvcmeteo.models.ForecastModel;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import java.util.ArrayList;
+
 @Controller
 public class ForecastController {
     @GetMapping("/")
     public ModelAndView index() {
         ModelAndView modelAndView = new ModelAndView("index");
+
+        var forecasts = new ArrayList<ForecastModel>();
+        var row1 = new ForecastModel("2024-03-12 11:00", 1.0);
+        var row2 = new ForecastModel("2024-03-12 12:00", 4.2);
+        var row3 = new ForecastModel("2024-03-12 13:00", 6.4);
+
+        forecasts.add(row1);
+        forecasts.add(row2);
+        forecasts.add(row3);
+
+        modelAndView.addObject("forecasts", forecasts);
+
         return modelAndView;
     }
 }
