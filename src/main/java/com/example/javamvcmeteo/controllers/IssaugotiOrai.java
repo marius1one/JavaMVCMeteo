ff9844ff4bba8570ee071170f12b32f43cbf4a8c
DESKTOP-AF5QAEV\ACER <marius.petrulis1@gmail.com>
2024-04-09 17:24:29 +0300

    Prideti issaugoti orai controller ir view

--- src/main/java/com/example/javamvcmeteo/controllers/IssaugotiOrai.java
+++ src/main/java/com/example/javamvcmeteo/controllers/IssaugotiOrai.java
@@ -1,4 +1,23 @@
 package com.example.javamvcmeteo.controllers;
 
+import com.example.javamvcmeteo.entities.ForecastEntity;
+import com.example.javamvcmeteo.repository.ForecastRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+
+@Controller
 public class IssaugotiOrai {
+    @Autowired
+    private ForecastRepository forecastRepository;
+
+    @GetMapping("/issaugotiOrai")
+    public String rodytiIssaugotusOrus(Model model) {
+        List<ForecastEntity> forecasts = forecastRepository.findAll();
+        model.addAttribute("forecasts", forecasts);
+        return "issaugotiOrai";
+    }
 }
--- src/main/java/com/example/javamvcmeteo/repository/ForecastRepository.java
+++ src/main/java/com/example/javamvcmeteo/repository/ForecastRepository.java
@@ -3,8 +3,10 @@ package com.example.javamvcmeteo.repository;
 import com.example.javamvcmeteo.entities.ForecastEntity;
 import org.springframework.data.repository.CrudRepository;
 
-public interface ForecastRepository extends CrudRepository<ForecastEntity, Integer>{
+import java.util.List;
 
+public interface ForecastRepository extends CrudRepository<ForecastEntity, Integer>{
+    List<ForecastEntity> findAll();
 
 
 }
